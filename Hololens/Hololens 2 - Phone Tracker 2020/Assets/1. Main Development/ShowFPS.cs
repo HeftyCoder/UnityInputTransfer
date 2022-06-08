0cfa89688fd12a6c95fe72a3cd34cdafefaf7e4f
salokinsomisa <salokinsomisa2@gmail.com>
2022-06-08 18:07:16 +0300

    Added QR Tracker from Nugget

--- Hololens/Hololens 2 - Phone Tracker 2020/Assets/1. Main Development/ShowFPS.cs
+++ Hololens/Hololens 2 - Phone Tracker 2020/Assets/1. Main Development/ShowFPS.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+
 public class ShowFPS : MonoBehaviour
 {
 
---
+++ Hololens/Hololens 2 - Phone Tracker 2020/Assets/1. Main Development/Tracking/QRTarget.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Microsoft.MixedReality.Toolkit;
+public class QRTarget : MonoBehaviour 
+{ 
+
+}
+
